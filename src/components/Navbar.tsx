ecf4b226c7a465261525d5c9c3a05bfd4a7360c3
fior <marcin.lukasz.fiorek@gmail.com>
2022-09-11 18:00:51 +0200

    Change navbar color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar: React.FC = () => {
   const menuClass = classNames({ 'scale-0 opacity-0': !menuOpen, 'scale-100 opacity-100': menuOpen });
 
   return (
-    <div className='flex items-center justify-between bg-gray-700 px-6 py-2'>
+    <div className='flex items-center justify-between bg-slate-700 px-6 py-2'>
       <h1 className='text-center text-2xl font-extrabold text-slate-200'>fior-t3-todo</h1>
       {session?.user && (
         <div className='relative' tabIndex={0} onFocus={() => setMenuOpen(true)} onBlur={() => setMenuOpen(false)}>
