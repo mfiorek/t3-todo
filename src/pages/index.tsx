4fbd541f63a2f51686f21ec73d46eb19d3b17548
fior <marcin.lukasz.fiorek@gmail.com>
2022-09-13 20:01:37 +0200

    Fix footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,8 +55,9 @@ const TaskPage: React.FC = () => {
             <TaskComponent key={task.id} task={task} />
           ))}
       </ul>
-      <div className='mt-4 flex justify-center gap-1 bg-slate-700 p-2 text-sm'>
-        Made for 🤪 by <a href='https://mfiorek.github.io/'>Marcin Fiorek Codes</a> 🥦 Source code: <a href='https://github.com/mfiorek/t3-todo'>github</a> ⌨
+      <div className='mt-4 flex flex-wrap justify-center gap-x-2 bg-slate-700 p-2 text-sm'>
+        <p className='text-center'>Made for 🤪 by <a href='https://mfiorek.github.io/'>Marcin Fiorek Codes</a> 🥦</p>
+        <p className='text-center'>Source code: <a href='https://github.com/mfiorek/t3-todo'>github</a> ⌨</p>
       </div>
     </>
   );
