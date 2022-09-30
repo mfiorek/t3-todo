48a44c51009feb308f8161b35e5ce11d9ac4ee7a
fior <marcin.lukasz.fiorek@gmail.com>
2022-09-30 20:34:20 +0200

    Add disable buttons and add loaders on login and logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 const Navbar: React.FC = () => {
   const { data: session } = useSession();
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const menuClass = classNames({ 'scale-0 opacity-0': !menuOpen, 'scale-100 opacity-100': menuOpen });
 
@@ -20,11 +21,11 @@ const Navbar: React.FC = () => {
         </div>
         {session?.user && (
           <div className='relative' tabIndex={0} onFocus={() => setMenuOpen(true)} onBlur={() => setMenuOpen(false)}>
-            <div className='relative h-10 w-10 cursor-pointer overflow-hidden rounded-full bg-slate-400 border'>
+            <div className='relative h-10 w-10 cursor-pointer overflow-hidden rounded-full border bg-slate-400'>
               {session.user.image ? (
                 <Image src={session.user.image} alt='user pic' layout='fill' />
               ) : (
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512' className='pt-2 h-full w-full' stroke='currentColor' fill='#1e293b'>
+                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512' className='h-full w-full pt-2' stroke='currentColor' fill='#1e293b'>
                   <path d='M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0S96 57.3 96 128s57.3 128 128 128zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z' />
                 </svg>
               )}
@@ -38,13 +39,20 @@ const Navbar: React.FC = () => {
               </div>
               <hr className='border-slate-500' />
               <button
-                className='w-full bg-red-500 p-1'
-                onFocus={() => {
+                className='flex w-full items-center justify-center gap-2 bg-red-500 p-1 disabled:cursor-not-allowed disabled:opacity-20'
+                disabled={isDisabled}
+                onClick={() => {
+                  setIsDisabled(true);
                   signOut();
                   setMenuOpen(true);
                 }}
               >
-                Logout
+                <p>Logout</p>
+                {isDisabled && (
+                  <svg xmlns='http://www.w3.org/2000/svg' className='h-4 w-4 animate-spin' viewBox='0 0 24 24' stroke='currentColor' fill='currentColor'>
+                    <path d='M12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8V2C6.579 2 2 6.58 2 12c0 5.421 4.579 10 10 10z' />
+                  </svg>
+                )}
               </button>
             </div>
           </div>
