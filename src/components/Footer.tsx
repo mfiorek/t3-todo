edbf1e4be3cfd5e40dae38a499159b3048110c97
fior <marcin.lukasz.fiorek@gmail.com>
2022-09-30 20:05:41 +0200

    Improve LoginPage

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-4 flex flex-wrap justify-center gap-x-2 bg-slate-700 p-2 text-sm'>
+      <p className='text-center'>
+        Made for 🤪 by <a href='https://mfiorek.github.io/'>Marcin Fiorek Codes</a> 🥦
+      </p>
+      <p className='text-center'>
+        Source code: <a href='https://github.com/mfiorek/t3-todo'>github</a> ⌨
+      </p>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ import { useAtom, useAtomValue } from 'jotai';
 import { mobileFocusRightAtom, selectedTaskListIdAtom } from '../state/atoms';
 import TaskListTitle from '../components/TaskListTitle';
 import { useRouter } from 'next/router';
+import Footer from '../components/Footer';
 
 const LoginPage: React.FC = () => {
   const { status } = useSession();
@@ -30,13 +31,42 @@ const LoginPage: React.FC = () => {
     );
   }
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center bg-slate-800'>
-      <Image src='/logo.svg' alt='logo' className='' width={180} height={180} />
-      <h1 className='text-center text-2xl font-extrabold'>Welcome to fior-t3-todo</h1>
-      <button onClick={() => signIn('github')} className='m-4 rounded-lg bg-slate-600 py-2 px-4 text-xl hover:bg-slate-500'>
-        Log in with GitHub!
-      </button>
-    </div>
+    <>
+      <div className='flex w-full grow flex-col items-center justify-center gap-8 bg-slate-800 lg:flex-row lg:gap-16'>
+        <section className='flex flex-col items-center'>
+          <Image src='/logo.svg' alt='logo' className='' width={180} height={180} />
+          <h1 className='text-4xl font-bold'>Login to</h1>
+          <h1 className='text-5xl font-extrabold'>fior-t3-todo</h1>
+        </section>
+        <section className='flex w-96 flex-col items-center gap-2 rounded-2xl bg-slate-700 p-6 font-medium shadow-2xl'>
+          <p className='text-2xl'>
+            Passwords are <strong>bad</strong>
+          </p>
+          <div className='text-center'>
+            <p>People forget passwords.</p>
+            <p>People steal passwords.</p>
+            <p>People lose passwords.</p>
+            <p>People pick bad passwords.</p>
+            <p>People reuse passwords.</p>
+          </div>
+          <div className='text-center text-lg'>
+            <p>You don&apos;t want another password.</p>
+            <p>Instead:</p>
+          </div>
+          <button
+            onClick={() => signIn('github')}
+            className='mt-4 flex w-full justify-center gap-2 rounded-lg bg-slate-300 fill-slate-800 py-2 px-4 text-xl font-semibold text-slate-800 shadow-xl transition-colors duration-300 hover:bg-slate-100'
+          >
+            <p>Log in with GitHub!</p>
+            <svg role='img' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' className='w-6'>
+              <title>GitHub</title>
+              <path d='M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12'></path>
+            </svg>
+          </button>
+        </section>
+      </div>
+      <Footer />
+    </>
   );
 };
 
@@ -111,14 +141,7 @@ const TaskPage: React.FC = () => {
           )}
         </div>
       </div>
-      <div className='mt-4 flex flex-wrap justify-center gap-x-2 bg-slate-700 p-2 text-sm'>
-        <p className='text-center'>
-          Made for 🤪 by <a href='https://mfiorek.github.io/'>Marcin Fiorek Codes</a> 🥦
-        </p>
-        <p className='text-center'>
-          Source code: <a href='https://github.com/mfiorek/t3-todo'>github</a> ⌨
-        </p>
-      </div>
+      <Footer />
     </>
   );
 };
